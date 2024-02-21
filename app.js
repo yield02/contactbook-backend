0fdfe0d07d7fbeb98c2f741093ccc4c0a4683497
thanhnhuong <thanhnhuong0702@gmail.com>
2024-02-21 15:13:03 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
